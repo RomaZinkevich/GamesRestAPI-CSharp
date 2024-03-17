f5e5b26d901a41700b80a6555a369ed17f56f33c
RomaZinkevich <zra1903@mail.ru>
2024-03-17 20:26:36 +0200

    added Delete and Put endpoints

--- TestWebApplication/Program.cs
+++ TestWebApplication/Program.cs
@@ -1,3 +1,4 @@
+using TestWebApplication;
 using TestWebApplication.Dtos;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -66,8 +67,10 @@ List<GameDto> games = [
 app.MapGet("games", () => games);
 
 // GET /games/{id}
-app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id))
-    .WithName(GetGameEndpointName);
+app.MapGet("games/{id}", (int id) => {
+    GameDto? game = games.Find(game => game.Id == id);
+    return game is null ? Results.NotFound() : Results.Ok(game);
+}).WithName(GetGameEndpointName);
 
 // POST /games
 app.MapPost("games", (CreateGameDto newGame) => {
@@ -82,5 +85,32 @@ app.MapPost("games", (CreateGameDto newGame) => {
 
     return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id}, game);
 });
+
+// PUT /games/{id}
+app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) => {
+    var index = games.FindIndex(game => game.Id == id);
+
+    if (index == -1) {
+        return Results.NotFound();
+    }
+
+    games[index] = new GameDto(
+        id,
+        updatedGame.Name,
+        updatedGame.Genre,
+        updatedGame.Price,
+        updatedGame.ReleaseDate
+    );
+
+    return Results.NoContent();
+});
+
+// DELETE /games/{id}
+app.MapDelete("games/{id}", (int id) => {
+    games.RemoveAll(game => game.Id == id);
+
+    return Results.NoContent();
+});
+
 
 app.Run();
--- TestWebApplication/obj/Debug/net8.0/TestWebApplication.AssemblyInfo.cs
+++ TestWebApplication/obj/Debug/net8.0/TestWebApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestWebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0a6f8c7471031e8a6a799fe2a7ea57abcd7bbf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4f69cdd7396fbd4fb510ee99c472b786c3ad7f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestWebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestWebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
